98ca67ff472b8f4b1faefc29ed96e2861e8008a5
Amar Dhebar <amar.dhebar.ad@gmail.com>
2018-05-01 18:47:44 -0500

    Initial commit

---
+++ app/src/main/java/amardhebardevelopments/cryptocurrencypricetool/Intropage.java
@@ -0,0 +1,21 @@
+package amardhebardevelopments.cryptocurrencypricetool;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class Intropage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intropage);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+
+
+    }
+
+}
